c77b558dff5e2cb69e480ed38c7c08c1429719a9
dnlklbv <hi.daniil.k@gmail.com>
2019-06-27 17:44:45 +0300

    feat: implement basic app layout

---
+++ piskel-clone/src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <div className="container">
+    <aside className="container__sidebar" />
+    <main className="container__main-section" />
+    <aside className="container__sidebar" />
+  </div>
+);
+
+export default App;

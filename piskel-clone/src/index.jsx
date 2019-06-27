3b13342231cd758692f34bf607b1d92367b01bab
dnlklbv <hi.daniil.k@gmail.com>
2019-06-27 18:01:59 +0300

    feat: set up redux

--- piskel-clone/src/index.jsx
+++ piskel-clone/src/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store/reducers';
+
 import App from './components/App';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );

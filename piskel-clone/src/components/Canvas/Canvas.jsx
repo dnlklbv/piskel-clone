a306724556d12e080882e0c98a9762f5e6a12a1b
dnlklbv <hi.daniil.k@gmail.com>
2019-06-27 20:32:17 +0300

    feat: prepare Canvas for drawing implementation

--- piskel-clone/src/components/Canvas/Canvas.jsx
+++ piskel-clone/src/components/Canvas/Canvas.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Canvas = () => (
-  <div className="canvas-wrapper">
-    <canvas className="canvas" />
-  </div>
-);
+class Canvas extends Component {
+  constructor(props) {
+    super(props);
+
+    this.isPainting = false;
+
+    this.startDraw = this.startDraw.bind(this);
+    this.continueDraw = this.continueDraw.bind(this);
+    this.stopDraw = this.stopDraw.bind(this);
+  }
+
+  componentDidMount() {
+    this.ctx = this.canvas.getContext('2d');
+  }
+
+  startDraw({ nativeEvent }) {
+    this.isPainting = true;
+
+    const { ctx } = this;
+    const { offsetX, offsetY } = nativeEvent;
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(offsetX, offsetY, 1, 1);
+  }
+
+  continueDraw({ nativeEvent }) {
+    if (!this.isPainting) return;
+    const { ctx } = this;
+    const { offsetX, offsetY } = nativeEvent;
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(offsetX, offsetY, 1, 1);
+  }
+
+  stopDraw() {
+    if (!this.isPainting) return;
+    this.isPainting = false;
+  }
+
+  render() {
+    return (
+      <div className="canvas-wrapper">
+        <canvas
+          className="canvas"
+          width="32"
+          height="32"
+          ref={(canvas) => { this.canvas = canvas; }}
+          onMouseDown={this.startDraw}
+          onMouseMove={this.continueDraw}
+          onMouseUp={this.stopDraw}
+          onMouseOut={this.stopDraw}
+          onBlur={this.stopDraw}
+        />
+      </div>
+    );
+  }
+}
 
 export default Canvas;

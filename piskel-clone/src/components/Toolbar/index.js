8b2b0852b295831816510cd7c3802e777eef19fc
dnlklbv <hi.daniil.k@gmail.com>
2019-06-29 20:03:06 +0300

    feat: add lighten tool button in toolbar

--- piskel-clone/src/components/Toolbar/index.js
+++ piskel-clone/src/components/Toolbar/index.js
@@ -7,5 +7,6 @@ import './img/icon-mirror-pen.png';
 import './img/icon-pen.png';
 import './img/icon-dithering.png';
 import './img/icon-fill-same.png';
+import './img/icon-lighten.png';
 
 export default ToolbarContainer;

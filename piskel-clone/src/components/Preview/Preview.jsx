a4294b6d613bc1809886d665cdcc24398aeab310
dnlklbv <hi.daniil.k@gmail.com>
2019-07-02 16:57:53 +0300

    feat: implement fullscreen for preview

--- piskel-clone/src/components/Preview/Preview.jsx
+++ piskel-clone/src/components/Preview/Preview.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 
-const Preview = () => (
-  <div className="preview-wrapper">
-    <div className="preview">
-      <button type="button" className="preview__button" />
-    </div>
-    <div className="preview-control">
-      <p>FPS:</p>
-      <input className="preview-control__range" type="range" min="1" max="24" />
-      <p>12</p>
-    </div>
-  </div>
-);
+class Preview extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.isFullcreen = false;
+    this.toggleFullScreen = this.toggleFullScreen.bind(this);
+  }
+
+  toggleFullScreen() {
+    if (!this.isFullcreen) {
+      this.preview.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+    this.isFullcreen = !this.isFullcreen;
+  }
+
+  render() {
+    return (
+      <div className="preview-wrapper">
+        <div className="preview" ref={(preview) => { this.preview = preview; }}>
+          <button type="button" className="preview__button" onClick={this.toggleFullScreen} />
+        </div>
+        <div className="preview-control">
+          <p>FPS:</p>
+          <input className="preview-control__range" type="range" min="1" max="24" />
+          <p>12</p>
+        </div>
+      </div>
+    );
+  }
+}
 export default Preview;

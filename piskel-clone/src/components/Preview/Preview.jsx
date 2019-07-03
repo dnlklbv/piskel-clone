92d27b3f704733e3591d69e986ea5c81ce1dff98
dnlklbv <hi.daniil.k@gmail.com>
2019-07-03 13:26:17 +0300

    fix: fix incorrect fullscreen mode for preview

--- piskel-clone/src/components/Preview/Preview.jsx
+++ piskel-clone/src/components/Preview/Preview.jsx
@@ -6,29 +6,34 @@ class Preview extends React.Component {
   constructor(props) {
     super(props);
 
-    this.isFullcreen = false;
     this.toggleFullScreen = this.toggleFullScreen.bind(this);
     this.updateFPS = this.updateFPS.bind(this);
   }
 
   componentDidUpdate() {
     const { fps, frameList } = this.props;
-    let i = 0;
     clearInterval(this.intervalId);
+    this.preview.style.backgroundImage = `url(${frameList[0] ? frameList[0] : ''})`;
+    if (frameList.length < 2) return;
+    let i = 1;
     this.intervalId = setInterval(() => {
       i += 1;
       if (i === frameList.length) i = 0;
-      this.preview.style.backgroundImage = `url(${frameList[i]})`;
+      this.preview.style.backgroundImage = `url(${frameList[i] ? frameList[i] : ''})`;
     }, Math.round(1000 / fps));
   }
 
   toggleFullScreen() {
-    if (!this.isFullcreen) {
+    // check if fullscreen mode enabled
+    const fullscreenElement = document.fullscreenElement
+      || document.mozFullScreenElement
+      || document.webkitFullscreenElement;
+
+    if (!fullscreenElement) {
       this.preview.requestFullscreen();
     } else {
       document.exitFullscreen();
     }
-    this.isFullcreen = !this.isFullcreen;
   }
 
   updateFPS(event) {
@@ -39,16 +44,18 @@ class Preview extends React.Component {
   render() {
     const { fps } = this.props;
     return (
-      <div className="preview-wrapper">
-        <div className="preview" ref={(preview) => { this.preview = preview; }}>
-          <button type="button" className="preview__button" onClick={this.toggleFullScreen} />
+      <>
+        <div className="preview-wrapper">
+          <div className="preview" ref={(preview) => { this.preview = preview; }}>
+            <button type="button" className="preview__button" onClick={this.toggleFullScreen} />
+          </div>
         </div>
         <div className="preview-control">
           <p>FPS:</p>
           <input className="preview-control__range" type="range" min="1" max="24" value={fps} onChange={this.updateFPS} />
           <p>{fps}</p>
         </div>
-      </div>
+      </>
     );
   }
 }
